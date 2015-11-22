fabcb706da3d816a4a7804a7687eb7a88c29a5c7
SSharshunov <s.sharshunov@gmail.com>
2015-11-22 21:27:02 +0500

    Update isValid function

--- src/MailCheck/mailchecker.php
+++ src/MailCheck/mailchecker.php
@@ -6,7 +6,15 @@ class mailchecker
 {
     public static function isValid($service, $user, $pass)
     {
-		
+		if (!$pass) {
+			throw new Exception('Something wrong.');
+		}
+		else 
+			return 1; //Passwd true
         return 'Hello World, Composer!';
     }
+	
+	
+    
+}
 }
